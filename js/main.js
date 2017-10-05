94e4f4c3648258487e2529b876c6ab035ad208cc
Zeyad Etman <zeyadetman@gmail.com>
2017-10-05 21:54:09 +0200

    new design

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-$(window).load(function() {
-    $('.preloader').fadeOut(1000); // set duration in brackets 
-});
-
-$(document).ready(function() {
-    $('#fullpage').fullpage({
-        //sectionsColor: ['#1bbc9b', '#4BBFC3', '#B92B27', 'whitesmoke', '#ccddff'],
-        anchors: ['home', 'projects', 'send-project', 'about'],
-        menu: '#menu',
-        scrollingSpeed: 1000
-    });
-});
+window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    t._e = [];
+    t.ready = function(f) {
+        t._e.push(f);
+    };
+    return t;
+}(document, "script", "twitter-wjs"));
 
 
 var text = "";
@@ -89,9 +91,6 @@ var project = [{
     }
 ];
 
-
-selectt();
-
 var javascriptlist = [];
 var javalist = [];
 var csharplist = [];
@@ -115,95 +114,32 @@ function organize(projects) {
         }
     }
     console.log(javascriptlist, javalist, csharplist, pythonlist, cpplist);
-}
+    addProjs(javascriptlist, 'javascript');
+    addProjs(javalist, 'java');
+    addProjs(csharplist, 'csharp');
+    addProjs(pythonlist, 'python');
+    addProjs(cpplist, 'cpp');
 
+
+}
 
 res2 = project;
 var curproj = 0;
 
-
-function next(lang) {
-    console.log(javascriptlist, javalist, csharplist, pythonlist, cpplist);
-    var language = lang + 'list';
-    var prolist = [];
-    if (language == 'javascriptlist') prolist = javascriptlist;
-    else if (language == 'javalist') prolist = javalist;
-    else if (language == 'csharplist') prolist = csharplist;
-    else if (language == 'pythonlist') prolist = pythonlist;
-    else if (language == 'cpplist') prolist = cpplist;
-    var tweet = `#tweet${lang}`;
-    var ii = Math.floor(Math.random() * (prolist.length - 0) + 0);
-    //console.log(prolist, prolist.length, ii);
-    $(`#${lang}a`).attr('href', prolist[ii]['Link']);
-    $(`#${lang}a`).html(prolist[ii]['Name']);
-    $(`#${lang}p`).html(prolist[ii]['Description']);
-    while (document.getElementsByClassName(`twitter${lang}`)[0].hasChildNodes()) {
-        document.getElementsByClassName(`twitter${lang}`)[0].removeChild(document.getElementsByClassName(`twitter${lang}`)[0].lastChild);
+function addProjs(arr, lang) {
+    for (var i = 0; i < arr.length; i++) {
+        $('.container').append(`
+        <div class="col-md-2 proj ${lang}">
+        <h4><a href="${arr[i].Link}" target="_blank">${arr[i].Name}</a></h4>
+        <span>${arr[i].Language}</span>
+        <p>${arr[i].Description}</p>
+        <ul class="text-center social">
+        <li><a class="twitter-share-button" href="https://twitter.com/share" data-text="Check this project" data-url="${ arr[i].Link}" data-hashtags="counteributors" data-via="counteributors">Tweet</a>
+        </li>
+        <li><a class="github-button" href="${arr[i].Link+'/fork'}" data-icon="octicon-repo-forked" data-show-count="false" aria-label="Fork ${arr[i].Name} on GitHub" target="_blank">Fork</a>
+        </li>
+        </ul>
+        </div>
+        `);
     }
-    $(`.twitter${lang}`).append(`<a id="tweet${lang}" class="twitter-share-button" href="https://twitter.com/share" data-text="Check this project" data-url="${ prolist[ii].Link}" data-hashtags="counteributors" data-via="counteributors">Tweet</a>`);
-    twttr.widgets.load();
-
-    while (document.getElementsByClassName(`github${lang}`)[0].hasChildNodes()) {
-        document.getElementsByClassName(`github${lang}`)[0].removeChild(document.getElementsByClassName(`github${lang}`)[0].lastChild);
-    }
-    $(`.github${lang}`).append(`<a class="github-button" href="${prolist[ii].Link+'/fork'}" data-icon="octicon-repo-forked" data-show-count="true" aria-label="Fork ${prolist[ii].Name} on GitHub" target="_blank">Fork</a>`);
-    (function() {
-        var js = document.createElement("script");
-        js.src = (/^http:/.test(document.location) ? "http" : "https") + "://buttons.github.io/buttons.js";
-        $(js).attr('async defer');
-        document.getElementsByTagName("head")[0].appendChild(js);
-    })();
-    //console.log(prolist[ii].Link);
-}
-
-function selectt() {
-    var jsflag = false;
-    var jflag = false;
-    var pythonflag = false;
-    var cppflag = false;
-    var csflag = false;
-    for (var i = 0; i < project.length; i++) {
-        if (project[i].Language == 'javascript' && jsflag == false) {
-            $('#javascripta').attr('href', project[i]['Link']);
-            $('#javascripta').html(project[i]['Name']);
-            $('#javascriptp').html(project[i]['Description']);
-            $(".twitterjavascript").append('<a id="tweetjavscript" class="twitter-share-button" href="https://twitter.com/share" data-text="Check this project" data-url="' + project[i].Link + '" data-hashtags="counteributors" data-via="counteributors">Tweet</a>');
-            $('.githubjavascript').append(`<a class="github-button" href="${project[i].Link+'/fork'}" data-icon="octicon-repo-forked" data-show-count="true" aria-label="Fork ${project[i].Name} on GitHub">Fork</a>`);
-            jsflag = true;
-        } else if (project[i].Language == 'java' && jflag == false) {
-            $('#javaa').attr('href', project[i]['Link']);
-            $('#javaa').html(project[i]['Name']);
-            $('#javap').html(project[i]['Description']);
-            $(".twitterjava").append('<a id="tweetjava" class="twitter-share-button" href="https://twitter.com/share" data-text="Check this project" data-url="' + project[i].Link + '" data-hashtags="counteributors" data-via="counteributors">Tweet</a>');
-            $('.githubjava').append(`<a class="github-button" href="${project[i].Link+'/fork'}" data-icon="octicon-repo-forked" data-show-count="true" aria-label="Fork ${project[i].Name} on GitHub">Fork</a>`);
-            jflag = true;
-        } else if (project[i].Language == 'csharp' && csflag == false) {
-            $('#csharpa').attr('href', project[i]['Link']);
-            $('#csharpa').html(project[i]['Name']);
-            $('#csharpp').html(project[i]['Description']);
-            $(".twittercsharp").append('<a id="tweetcsharp" class="twitter-share-button" href="https://twitter.com/share" data-text="Check this project" data-url="' + project[i].Link + '" data-hashtags="counteributors" data-via="counteributors">Tweet</a>');
-            $('.githubcsharp').append(`<a class="github-button" href="${project[i].Link+'/fork'}" data-icon="octicon-repo-forked" data-show-count="true" aria-label="Fork ${project[i].Name} on GitHub">Fork</a>`);
-            csflag = true;
-        } else if (project[i].Language == 'python' && pythonflag == false) {
-            $('#pythona').attr('href', project[i]['Link']);
-            $('#pythona').html(project[i]['Name']);
-            $('#pythonp').html(project[i]['Description']);
-            $(".twitterpython").append('<a id="tweetpython" class="twitter-share-button" href="https://twitter.com/share" data-text="Check this project" data-url="' + project[i].Link + '" data-hashtags="counteributors" data-via="counteributors">Tweet</a>');
-            $('.githubpython').append(`<a class="github-button" href="${project[i].Link+'/fork'}" data-icon="octicon-repo-forked" data-show-count="true" aria-label="Fork ${project[i].Name} on GitHub">Fork</a>`);
-            pythonflag = true;
-        } else if (project[i].Language == 'cpp' && cppflag == false) {
-            $('#cppa').attr('href', project[i]['Link']);
-            $('#cppa').html(project[i]['Name']);
-            $('#cppp').html(project[i]['Description']);
-            $(".twittercpp").append('<a id="tweetcpp" class="twitter-share-button" href="https://twitter.com/share" data-text="Check this project" data-url="' + project[i].Link + '" data-hashtags="counteributors" data-via="counteributors">Tweet</a>');
-            $('.githubcpp').append(`<a class="github-button" href="${project[i].Link+'/fork'}" data-icon="octicon-repo-forked" data-show-count="true" aria-label="Fork ${project[i].Name} on GitHub">Fork</a>`);
-            cppflag = true;
-        }
-    }
-    (function() {
-        var js = document.createElement("script");
-        js.src = (/^http:/.test(document.location) ? "http" : "https") + "://buttons.github.io/buttons.js";
-        $(js).attr('async defer');
-        document.getElementsByTagName("head")[0].appendChild(js);
-    })();
-}
+};
